4f16e6893b00376a45d3648b23342b899205dd3b
Shotamax95 <shota.110.free.style@gmail.com>
2020-10-07 17:00:56 -0400

    1st commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,15 @@
+/*
+File: app.js
+Name: Shota Ito
+Student#: 301103095
+Date: Oct 7th 2020
+*/
+
+(function(){
+
+    function Start(){
+    }
+
+    window.addEventListener("load", Start);
+    
+})();
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+/*
+File: index.js
+Name: Shota Ito
+Student#: 301103095
+Date: Oct 7th 2020
+*/
+
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('content/about', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('content/projects', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('content/services', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+
+module.exports = router;
