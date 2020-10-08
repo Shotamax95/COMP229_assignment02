a01e296761e77c78321d7b480ab3c553be3bfbaf
Shotamax95 <shota.110.free.style@gmail.com>
2020-10-08 12:10:01 -0400

    Update

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-app.use( express.static( 'public' ) );
 
 app.use('/', indexRouter);
 
